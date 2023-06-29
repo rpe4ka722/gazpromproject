c10d81c61af9e554de0c174f2ab474b90e5f3c12
rpe4ka722 <82370392+rpe4ka722@users.noreply.github.com>
2023-06-29 19:44:29 +0500

    Norm

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Department
+from account.models import Userprofile
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Userprofile)
